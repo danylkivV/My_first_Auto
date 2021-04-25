dfa6f8ac2b1aea6905e5f343e5b250d3f604b3d1
danylkivV <82308472+danylkivV@users.noreply.github.com>
2021-04-25 13:53:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,7 @@
-# # Script 1
-# from selenium import webdriver
-# import datetime
-# import os
-#
-# date_string = datetime.datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
-# cwd = os.getcwd()
-# image_folder = "site/y8.com"
-# image_path = os.path.join(cwd, image_folder)
-#
-# if not os.path.exists(image_path):
-#     os.makedirs(image_path)
-#
-# path = os.path.join(image_path,"y8_"+ date_string + ".png")
-# driver = webdriver.Chrome('./chromedriver')
-# driver.get("https://www.y8.com/")
-#
-# driver.save_screenshot(path)
-# driver.close()
-#
-#
-#
-# # Script 2
-# from selenium import webdriver
-# import datetime
-# import os
-# import time
-#
-# date_string = datetime.datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
-# cwd = os.getcwd()
-# image_folder = "site/y8.com"
-# image_path = os.path.join(cwd, image_folder)
-#
-# if not os.path.exists(image_path):
-#     os.makedirs(image_path)
-#
-# driver = webdriver.Chrome()
-# driver.get("http://www.y8.com")
-# driver.maximize_window()
-#
-# try:
-#     all_elems = driver.find_elements_by_css_selector('#items_container > div')
-#
-#     for index, _ in enumerate(all_elems, 1):
-#         css_path = "#items_container > div:nth-of-type({0})"
-#         elem = driver.find_element_by_css_selector(css_path.format(index))
-#         if elem.is_displayed():
-#             elem.click()
-#             time.sleep(20)
-#             name=driver.title
-#             path = os.path.join(image_path, name + date_string + ".png")
-#             driver.save_screenshot(path)
-#             driver.back()
-# finally:
-#     driver.close()
-
-# Script 3
+# Level 1
 from selenium import webdriver
 import datetime
+import time
 import os
 
 date_string = datetime.datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
@@ -67,9 +12,54 @@ image_path = os.path.join(cwd, image_folder)
 if not os.path.exists(image_path):
     os.makedirs(image_path)
 
+path = os.path.join(image_path, "y8_" + date_string + ".png")
+driver = webdriver.Chrome('./chromedriver')
+driver.get("https://www.y8.com/")
+
+driver.save_screenshot(path)
+driver.close()
+
+# Level 2
+date_string = datetime.datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+cwd = os.getcwd()
+image_folder = "site/y8.com"
+image_path = os.path.join(cwd, image_folder)
+
+if not os.path.exists(image_path):
+    os.makedirs(image_path)
+
 driver = webdriver.Chrome()
 driver.get("http://www.y8.com")
-#driver.maximize_window()
+driver.maximize_window()
+
+try:
+    all_elems = driver.find_elements_by_css_selector('#items_container > div')
+
+    for index, _ in enumerate(all_elems, 1):
+        css_path = "#items_container > div:nth-of-type({0})"
+        elem = driver.find_element_by_css_selector(css_path.format(index))
+        if elem.is_displayed():
+            elem.click()
+            time.sleep(15)
+            name = driver.title
+            path = os.path.join(image_path, name + date_string + ".png")
+            driver.save_screenshot(path)
+            driver.back()
+finally:
+    driver.close()
+
+# Level 3
+date_string = datetime.datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+cwd = os.getcwd()
+image_folder = "site/y8.com"
+image_path = os.path.join(cwd, image_folder)
+
+if not os.path.exists(image_path):
+    os.makedirs(image_path)
+
+driver = webdriver.Chrome()
+driver.get("http://www.y8.com")
+# driver.maximize_window()
 
 try:
     all_elems = driver.find_elements_by_css_selector('#items_container > div')
@@ -78,7 +68,7 @@ try:
         elem = driver.find_element_by_css_selector(css_path.format(index))
         if elem.is_displayed():
             elem.click()
-           # time.sleep(20)
+            time.sleep(15)
             name = driver.title
             path = os.path.join(image_path, name + date_string + ".png")
             driver.save_screenshot(path)
